dced8f2592c50a7d7f1257faad5454688ae17b80
Lamont Crook <lamont@adobe.com>
2023-05-05 13:41:53 -0600

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './screens/home/home';
