a7e529853dca60bbd968cee88cb14af162a01e3e
Lamont Crook <lamont@adobe.com>
2023-09-12 14:43:38 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Home from './screens/home/home';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const App = () => {
+  const context = {
+    endpoint: '/graphql/execute.json',
+    url: <AEM URL>,
+    project: 'pure-headless',
+  };
+
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route exact={true} path={'/'} element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <HelmetProvider>
+        <Helmet>
+          <meta name='urn:auecon:aemconnection' content={`aem:${context.url}`} />
+        </Helmet>
+        <BrowserRouter>
+          <Routes>
+            <Route exact={true} path={'/'} element={<Home context={context}/>} />
+          </Routes>
+        </BrowserRouter>
+      </HelmetProvider>
     </div>
   );
 };
